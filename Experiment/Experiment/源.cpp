0ad89f69086d7335fe38f8386d64690c13c7ff27
MyOptimusPrime <longzhucen@gmail.com>
2017-11-14 15:41:33 +0800

    Let program acts as a class

--- Experiment/Experiment/源.cpp
+++ Experiment/Experiment/源.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 
 // 总共的人数
-const static int person = 20;
+const static int person = 1000;
 
 // 每个人的需求数
 const static int per_request = 10;
@@ -38,19 +38,40 @@ double C = 1;
 double cur_sa_sum, cur_con_sum;
 double cur_sa_sum_2;
 vector<double> cur_sa_per_sum;
+vector<bool> gamer;
+vector<pair<int, int> > Data;
+ll sum_consume;
 
-void initialize(const vector<pair<int, int> > & data, const vector<bool> & gamer, int PER_REQUEST) {
+void initialize() {
+	cur_sa_sum = cur_con_sum = cur_sa_sum_2 = 0;
+	cur_sa_per_sum.clear();
+	cur_sa_per_sum.resize(person);
+	gamer.clear();
+	for (int i = 0; i < person*per_request; i++) {
+		//gamer.pb(false);
+		gamer.pb(true);
+	}
 	int n = gamer.size();
 	for (int i = 0; i < n; i++) {
-		cur_sa_sum += gamer[i] ? data[i].first : 0;
-		cur_con_sum += gamer[i] ? data[i].second : 0;
-		cur_sa_per_sum[i / PER_REQUEST] += gamer[i] ? data[i].first : 0;
+		cur_sa_sum += gamer[i] ? Data[i].first : 0;
+		cur_con_sum += gamer[i] ? Data[i].second : 0;
+		cur_sa_per_sum[i / per_request] += gamer[i] ? Data[i].first : 0;
 	}
-	for (int i = 0; i < n / PER_REQUEST; i++)
+	for (int i = 0; i < n / per_request; i++)
 		cur_sa_sum_2 += cur_sa_per_sum[i] * cur_sa_per_sum[i];
 }
 
-bool generate_rand(int PERSON, int PER_REQUEST) {
+void update(int i) {
+	cur_sa_sum -= gamer[i] ? Data[i].first : -Data[i].first;
+	cur_con_sum -= gamer[i] ? Data[i].second : -Data[i].second;
+
+	cur_sa_sum_2 -= cur_sa_per_sum[i / per_request] * cur_sa_per_sum[i / per_request];
+	cur_sa_per_sum[i / per_request] -= gamer[i] ? Data[i].first : -Data[i].first;
+	cur_sa_sum_2 += cur_sa_per_sum[i / per_request] * cur_sa_per_sum[i / per_request];
+	gamer[i] = !gamer[i];
+}
+
+bool generate_rand() {
 
 	ofstream out("rand.txt");
 
@@ -58,11 +79,11 @@ bool generate_rand(int PERSON, int PER_REQUEST) {
 
 	srand(time(NULL));
 
-	for (int per = 1; per <= PERSON; ++per) {
+	for (int per = 1; per <= person; ++per) {
 		vector<pair<int, int> > ev;
 		ev.clear();
 		int cur_sum = 0;
-		for (int req = 1; req <= PER_REQUEST; ++req) {
+		for (int req = 1; req <= per_request; ++req) {
 			int satisfy = rand() % (SUM_SATISFY - 20) + 20; //
 			int consume = rand() % MAX_CONSUME + 1; // 1 - MAX_CONSUME
 			cur_sum += satisfy;
@@ -70,9 +91,9 @@ bool generate_rand(int PERSON, int PER_REQUEST) {
 		}
 
 		int left = SUM_SATISFY;
-		for (int req = 1; req <= PER_REQUEST; ++req) {
+		for (int req = 1; req <= per_request; ++req) {
 			int satisfy = int(ev[req - 1].first * 100.0 / cur_sum + 0.5);
-			if (req == PER_REQUEST) {
+			if (req == per_request) {
 				satisfy = left;
 			}
 			ev[req - 1].first = satisfy;
@@ -80,7 +101,7 @@ bool generate_rand(int PERSON, int PER_REQUEST) {
 			left -= satisfy;
 		}
 		sort(ev.begin(), ev.end());
-		for (int req = PER_REQUEST - 1; req >= 0; --req) {
+		for (int req = per_request - 1; req >= 0; --req) {
 		//for (int req = 0; req <= PER_REQUEST - 1; ++req) {
 			out << ev[req].first << '\t' << ev[req].second << endl;
 		}
@@ -89,45 +110,33 @@ bool generate_rand(int PERSON, int PER_REQUEST) {
 	return true;
 }
 
-
-vector<pair<int, int> > read_data(ll &consume_sum) {
-	vector<pair<int, int> > ans;
-	ans.clear();
+void read_data() {
+	Data.clear();
 	ifstream in("rand.txt");
-	if (!in.is_open()) return ans;
+	if (!in.is_open()) return;
 	int satisfy, consume;
-	consume_sum = 0;
+	sum_consume = 0;
 	while (in >> satisfy >> consume) {
-		ans.pb(mp(satisfy, consume));
-		consume_sum += (ll)consume;
+		Data.pb(mp(satisfy, consume));
+		sum_consume += (ll)consume;
 	}
-	return ans;
+	return;
 }
 
-double calc_p(const vector<pair<int, int> > & data, int sum_consume, int PERSON, int PER_REQUEST) {
-	double varia = cur_sa_sum_2 / PERSON - cur_sa_sum * cur_sa_sum / PERSON / PERSON;
-	return A * cur_sa_sum / PERSON / 100 - B / 2500 * varia - C * cur_con_sum / sum_consume;
+double calc_p() {
+	double varia = cur_sa_sum_2 / person - cur_sa_sum * cur_sa_sum / person / person;
+	return A * cur_sa_sum / person / 100 - B / 2500 * varia - C * cur_con_sum / sum_consume;
 }
 
-double check_pimax(const vector<pair<int, int> > & data, bool gameri, int i, int sum_consume, int PERSON, int PER_REQUEST) {
-	double p_former = calc_p(data, sum_consume, PERSON, PER_REQUEST);
+double check_pimax(int i) {
+	double p_former = calc_p();
 
-	cur_sa_sum -= gameri ? data[i].first : -data[i].first;
-	cur_con_sum -= gameri ? data[i].second : -data[i].second;
+	update(i);
 
-	cur_sa_sum_2 -= cur_sa_per_sum[i / PER_REQUEST] * cur_sa_per_sum[i / PER_REQUEST];
-	cur_sa_per_sum[i / PER_REQUEST] -= gameri ? data[i].first : -data[i].first;
-	cur_sa_sum_2 += cur_sa_per_sum[i / PER_REQUEST] * cur_sa_per_sum[i / PER_REQUEST];
-
-	double p_later = calc_p(data, sum_consume, PERSON, PER_REQUEST);
+	double p_later = calc_p();
 
 	// recover
-	cur_sa_sum += gameri ? data[i].first : -data[i].first;
-	cur_con_sum += gameri ? data[i].second : -data[i].second;
-
-	cur_sa_sum_2 -= cur_sa_per_sum[i / PER_REQUEST] * cur_sa_per_sum[i / PER_REQUEST];
-	cur_sa_per_sum[i / PER_REQUEST] += gameri ? data[i].first : -data[i].first;
-	cur_sa_sum_2 += cur_sa_per_sum[i / PER_REQUEST] * cur_sa_per_sum[i / PER_REQUEST];
+	update(i);
 
 	if (p_former > p_later)
 		return -10;
@@ -135,50 +144,17 @@ double check_pimax(const vector<pair<int, int> > & data, bool gameri, int i, int
 		return p_later;
 }
 
-int main() {
-	//产生随机矩阵
-	if (!generate_rand(person, per_request)) {
-		cout << "Generate rand matrix failed" << endl;
-		return 0;
-	}
-	//读取随机矩阵
-
-	ll sum_consume = 0;
-	vector<pair<int, int> > data = read_data(sum_consume);
-	//寻找过程
-
-	//初始化
-	vector<bool> gamer;
-	gamer.clear();
-	for (int i = 0; i < person*per_request; i++) {
-		//gamer.pb(false);
-		gamer.pb(true);
-	}
-	bool flag = false;
+void run_game() {
+    bool flag = false;
 	long long step = 0;
 	double result_tmp = 0;
-	cur_sa_sum = cur_con_sum = cur_sa_sum_2 = 0;
-	cur_sa_per_sum.clear();
-	cur_sa_per_sum.resize(person);
-	initialize(data, gamer, per_request);
-
-	//主循环迭代
+	double tmp = 0;
 	while (!flag) {
 		flag = true;
 		for (int i = 0; i < person * per_request; i++) {
-			double tmp = check_pimax(data, gamer[i], i, sum_consume, person, per_request);
+			tmp = check_pimax(i);
 			if (tmp > -9) {
-
-				cur_sa_sum -= gamer[i] ? data[i].first : -data[i].first;
-				cur_con_sum -= gamer[i] ? data[i].second : -data[i].second;
-
-				cur_sa_sum_2 -= cur_sa_per_sum[i / per_request] * cur_sa_per_sum[i / per_request];
-				cur_sa_per_sum[i / per_request] -= gamer[i] ? data[i].first : -data[i].first;
-				cur_sa_sum_2 += cur_sa_per_sum[i / per_request] * cur_sa_per_sum[i / per_request];
-
-				gamer[i] = !gamer[i];
-				int tmpgamer = gamer[i];
-				tmpgamer = tmpgamer * 2 - 1;
+				update(i);
 				step++;
 				flag = false;
 				if (tmp - result_tmp > 0.01) {
@@ -188,8 +164,22 @@ int main() {
 			}
 		}
 	}
-	double result = calc_p(data, sum_consume, person, per_request);
-	cout << result << endl;
+}
+
+int main() {
+	//产生随机矩阵
+	if (!generate_rand()) {
+		cout << "Generate rand matrix failed" << endl;
+		return 0;
+	}
+
+	read_data();//读取随机矩阵
+
+	initialize();//初始化
+
+	run_game();//博弈运行
+
+	cout << calc_p() << endl;
 	getchar();
 	return 0;
 }
